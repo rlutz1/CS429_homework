8a65febda6a73a208ab663d3a02029914c395f06
Roxanne Krause <rlutz1@unm.edu>
2026-02-03 19:35:22 -0700

    okay, had to do with the learning rate being WAY to high in code given, now getting a reasonable plot

--- HW_1/adaline.py
+++ HW_1/adaline.py
@@ -45,26 +45,32 @@ class AdalineGD:
     -------
     self : object
     """
+    # same general initialization as perceptron
     rgen = np.random.RandomState(self.random_state)
-    self.w_ = rgen.normal(loc=0.0, scale=0.01,
-    size=X.shape[1])
-    self.b_ = np.float_(0.)
-    self.losses_ = []
-    for i in range(self.n_iter):
-      net_input = self.net_input(X)
+    self.w_ = rgen.normal(loc=0.0, scale=0.01,size=X.shape[1])
+    self.b_ = np.float64(0.)
+    self.losses_ = [] # errors versus losses? 
+
+    for i in range(self.n_iter): # for how many iterations
+      net_input = self.net_input(X) # z = wx + b
+      # "prediction" is now production of activation func
+      # which is for adaline the identity (return z)
       output = self.activation(net_input)
-      errors = (y - output)
-      self.w_ += self.eta * 2.0 * X.T.dot(errors) / X.shape[0]
-      self.b_ += self.eta * 2.0 * errors.mean()
-      loss = (errors**2).mean()
-      self.losses_.append(loss)
+      errors = (y - output) # error between real label and prediction
+      self.w_ += self.eta * 2.0 * X.T.dot(errors) / X.shape[0] # update function!
+      self.b_ += self.eta * 2.0 * errors.mean() # update function!
+      loss = (errors**2).mean() # loss is the mean squared error objective func!
+      self.losses_.append(loss) # track the loss
     return self
+  
   def net_input(self, X):
     """Calculate net input"""
     return np.dot(X, self.w_) + self.b_
+  
   def activation(self, X):
     """Compute linear activation"""
     return X
+  
   def predict(self, X):
     """Return class label after unit step"""
     return np.where(self.activation(self.net_input(X)) >= 0.5, 1, 0)
