5f8a9f8fabd75f0ddacd81756a41cb24b5b939b4
Roxanne Krause <rlutz1@unm.edu>
2026-02-03 19:02:17 -0700

    pushing a new empty py for adaline class.

---
+++ HW_1/adaline.py
@@ -0,0 +1,6 @@
+"""
+textbook:
+the Adaline algorithm compares the true class labels with the linear activation
+function's continuous valued output to compute the model error and update the weights. 
+In contrast, the perceptron compares the true class labels to the predicted class labels
+"""
