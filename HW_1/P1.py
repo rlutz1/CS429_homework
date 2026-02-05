c3340b860c67eb1f0060ddd5bcd4b5444d20f6ab
Roxanne Krause <rlutz1@unm.edu>
2026-02-05 06:44:13 -0700

    setting up for actual hw now that daddy has a better feeling as to what the hell is going on

---
+++ HW_1/P1.py
@@ -0,0 +1,14 @@
+"""
+Roxanne Krause | 2/5/26 | CS429
+
+TASK
+Modify the classes AdalineGD and LogisticRegressionGD 
+in the textbook such that the bias
+data field b is absorbed by the weight vector w . 
+Your program is required to be compatible with the training
+programs in the textbook.
+
+REPORT:
+Explain how the bias is transformed to an extra weight and why the translated model is equivalent
+to the original one.
+"""
